98482667989269f89d697838def3ea89719b13a9
alpbetgamdel <144608056+alpbetgamdel@users.noreply.github.com>
2023-09-18 21:23:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,72 @@ funev = home.classList.contain("fun");
 homeb.addEventListener{"mousedown", function(e) {
   if (homev === true) {
     home.classList.toggle("home");
+    math.classList.toggle("math");
+    sci.classList.toggle("sci");
+    eng.classList.toggle("eng");
+    ran.classList.toggle("ran");
+    fun.classList.toggle("fun");
   } else {
     home.classList.toggle("homevalid");
+  }
+})};
+mathb.addEventListener{"mousedown", function(e) {
+  if (mathv === true) {
+    home.classList.toggle("homevalid");
+    math.classList.toggle("mathvalid");
+    sci.classList.toggle("sci");
+    eng.classList.toggle("eng");
+    ran.classList.toggle("ran");
+    fun.classList.toggle("fun");
+  } else {
+    math.classList.toggle("mathvalid");
+  }
+})};
+scib.addEventListener{"mousedown", function(e) {
+  if (sciv === true) {
+    home.classList.toggle("homevalid");
+    math.classList.toggle("math");
+    sci.classList.toggle("scivalid");
+    eng.classList.toggle("eng");
+    ran.classList.toggle("ran");
+    fun.classList.toggle("fun");
+  } else {
+    sci.classList.toggle("scivalid");
+  }
+})};
+engb.addEventListener{"mousedown", function(e) {
+  if (engv === true) {
+    home.classList.toggle("homevalid");
+    math.classList.toggle("math");
+    sci.classList.toggle("sci");
+    eng.classList.toggle("engvalid");
+    ran.classList.toggle("ran");
+    fun.classList.toggle("fun");
+  } else {
+    eng.classList.toggle("engvalid");
+  }
+})};
+ranb.addEventListener{"mousedown", function(e) {
+  if (ranv === true) {
+    home.classList.toggle("homevalid");
+    math.classList.toggle("math");
+    sci.classList.toggle("sci");
+    eng.classList.toggle("eng");
+    ran.classList.toggle("ranvalid");
+    fun.classList.toggle("fun");
+  } else {
+    ran.classList.toggle("ranvalid");
+  }
+})};
+funb.addEventListener{"mousedown", function(e) {
+  if (funv === true) {
+    home.classList.toggle("homevalid");
+    math.classList.toggle("math");
+    sci.classList.toggle("sci");
+    eng.classList.toggle("eng");
+    ran.classList.toggle("ran");
+    fun.classList.toggle("funvalid");
+  } else {
+    fun.classList.toggle("funvalid");
   }
 })};
